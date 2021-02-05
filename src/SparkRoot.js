37fd2607dfab3b5f9b8caaf6569b727afa866dcf
lucko <git@lucko.me>
2021-02-05 12:58:08 +0000

    Happy new year
    oops a bit late

--- src/SparkRoot.js
+++ src/SparkRoot.js
@@ -28,7 +28,7 @@ function Footer() {
     return (
         <div id="footer">
             <a href="https://github.com/lucko/spark">spark</a> and <a href="https://github.com/lucko/spark-viewer">spark-viewer</a> are based on WarmRoast by sk89q.<br />
-            Copyright &copy; 2020 <a href="https://github.com/lucko">lucko</a>, <a href="https://github.com/astei">astei</a> & spark contributors
+            Copyright &copy; 2018-2021 <a href="https://github.com/lucko">lucko</a>, <a href="https://github.com/astei">astei</a> & spark contributors
         </div>
     )
 }
