6f37f9e89e9a381d78e14359126a031f3e298413
Luck <git@lucko.me>
2022-01-05 19:34:55 +0000

    auto year in copyright notice

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 export default function Footer() {
+    const year = new Date().getFullYear().toString()
     return (
         <footer>
             <a href="https://github.com/lucko/spark">spark</a> and{' '}
             <a href="https://github.com/lucko/spark-viewer">spark-viewer</a> are
             based on WarmRoast by sk89q.
             <br />
-            Copyright &copy; 2018-2021{' '}
+            Copyright &copy; 2018-{year}{' '}
             <a href="https://github.com/lucko">lucko</a>,{' '}
             <a href="https://github.com/astei">astei</a> & spark contributors
         </footer>
