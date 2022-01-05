59d84a3e173b9e88de5f1dc58b97ba02ab202752
Kn0ax <42520461+Kn0ax@users.noreply.github.com>
2022-01-05 17:31:50 +0100

    Happy new year! :)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
             <a href="https://github.com/lucko/spark-viewer">spark-viewer</a> are
             based on WarmRoast by sk89q.
             <br />
-            Copyright &copy; 2018-2021{' '}
+            Copyright &copy; 2018-2022{' '}
             <a href="https://github.com/lucko">lucko</a>,{' '}
             <a href="https://github.com/astei">astei</a> & spark contributors
         </footer>
