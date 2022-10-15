fe13578f0c8cbbb75aad31f53f8de2ea03d70b78
Cubxity <27609129+Cubxity@users.noreply.github.com>
2022-10-15 17:33:29 +0200

    Fix HTML tag lang (#52)

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
     return (
-        <Html>
+        <Html lang="en">
             <Head>
                 <meta charSet="utf-8" />
                 <meta name="theme-color" content="#FFC93A" />
