64c43288cc830d8811985bddbfb8ecbac334efdf
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-07-01 20:31:06 -0400

    Change rendered avatar size to 24px instead of 20px
    Improves rendering of pixel-perfect avatars

--- src/viewer/common/components/Avatar.tsx
+++ src/viewer/common/components/Avatar.tsx
@@ -11,9 +11,9 @@ export default function Avatar({ user }: AvatarProps) {
     let avatarUrl;
     if (user.type === CommandSenderMetadata_Type.PLAYER) {
         const uuid = user.uniqueId.replace(/-/g, '');
-        avatarUrl = 'https://crafthead.net/helm/' + uuid + '/20.png';
+        avatarUrl = 'https://crafthead.net/helm/' + uuid + '/24.png';
     } else {
-        avatarUrl = 'https://crafthead.net/avatar/Console/20.png';
+        avatarUrl = 'https://crafthead.net/avatar/Console/24.png';
     }
 
     return <img src={avatarUrl} alt="" />;
