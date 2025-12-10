8635a848c0cd1a993b5af49ac6d5b42eb8304362
Luck <git@lucko.me>
2025-12-10 08:18:28 +0000

    Update MC versions on download page

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -235,21 +235,21 @@ const DownloadButtons = ({ artifacts }: { artifacts: ArtifactsMap }) => {
             <DownloadInfo
                 artifacts={artifacts}
                 name="Fabric"
-                comment="MC 1.21.10"
+                comment="MC 1.21.11"
                 artifact="fabric"
                 logo={fabricLogo}
             />
             <DownloadInfo
                 artifacts={artifacts}
                 name="NeoForge"
-                comment="MC 1.21.10"
+                comment="MC 1.21.11"
                 artifact="neoforge"
                 logo={neoForgeLogo}
             />
             <DownloadInfo
                 artifacts={artifacts}
                 name="Forge"
-                comment="MC 1.21.10"
+                comment="MC 1.21.11"
                 artifact="forge"
                 logo={forgeLogo}
             />
